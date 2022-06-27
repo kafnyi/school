a07b6295c0d2dbbfbf4bcdec9189a1d927e3f06d
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-06-27 21:39:31 +0200

    getSearchResponseList method moved to service

--- src/main/java/hu/wurfel/refference/school/division/ClassService.java
+++ src/main/java/hu/wurfel/refference/school/division/ClassService.java
@@ -1,6 +1,7 @@
 package hu.wurfel.refference.school.division;
 
 import hu.wurfel.refference.school.base.enums.EntityFieldNames;
+import hu.wurfel.refference.school.base.enums.EntityNames;
 import hu.wurfel.refference.school.diary.Diary;
 import hu.wurfel.refference.school.diary.DiaryCrudService;
 import hu.wurfel.refference.school.mark.Mark;
@@ -37,6 +38,32 @@ public class ClassService extends ClassCrudService {
         this.teacherCrudService = teacherCrudService;
     }
 
+    List<Class> getSearchResponseList(EntityNames searchWith, EntityFieldNames searchBy, String value) {
+
+        switch (searchWith) {
+            case Student -> {
+                return searchWithStudent(searchBy, value);
+            }
+            case Diary -> {
+                return searchWithDiary(searchBy, value);
+            }
+            case Class -> {
+                return searchWithClass(searchBy, value);
+            }
+            case Subject -> {
+                return searchWithSubject(searchBy, value);
+            }
+            case Mark -> {
+                return searchWithMark(searchBy, value);
+            }
+            case Teacher -> {
+                return searchWithTeacher(searchBy, value);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
 
     List<Class> searchWithStudent(EntityFieldNames searchBy, String value) {
         switch (searchBy) {
