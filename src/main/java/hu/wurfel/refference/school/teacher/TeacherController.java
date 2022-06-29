5e3adffd6c8be6326baf01ecaf36d36c7833ec8e
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-06-29 19:22:28 +0200

    TeacherController @RequestMapping annotation added

--- src/main/java/hu/wurfel/refference/school/teacher/TeacherController.java
+++ src/main/java/hu/wurfel/refference/school/teacher/TeacherController.java
@@ -9,26 +9,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/Teacher/")
 public class TeacherController {
 
     TeacherService teacherService;
 
-    @GetMapping("/api/v1/Teacher/search/{searchWith}/{searchBy}/{value}")
+    @GetMapping("/search/{searchWith}/{searchBy}/{value}")
     public ResponseEntity<List<Teacher>> searchForStudent(@PathVariable EntityNames searchWith, @PathVariable EntityFieldNames searchBy, @PathVariable String value) {
         return ResponseEntity.ok(teacherService.getSearchResponseList(searchWith, searchBy, value));
     }
 
-    @PostMapping("/api/v1/Teacher/adding")
+    @PostMapping("/adding")
     public ResponseEntity<List<Teacher>> create(@RequestBody TeacherRequestForSearch teacherRequestForSearch) {
         return ResponseEntity.ok(teacherService.save(Long.parseLong(teacherRequestForSearch.getId()), teacherRequestForSearch.getName(), teacherRequestForSearch.getDate()));
     }
 
-    @DeleteMapping("/api/v1/Teacher/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable String id) {
         teacherService.delete(teacherService.getByTeacherId(Long.parseLong(id)));
     }
 
-    @PutMapping("/api/v1/Teacher/modify")
+    @PutMapping("/modify")
     public ResponseEntity<List<Teacher>> modifyStudent(@RequestBody TeacherRequestForSearch teacherRequestForSearch) {
         Teacher teacher = teacherService.getByTeacherId(Long.parseLong(teacherRequestForSearch.getId()));
         teacher.setName(teacherRequestForSearch.getName());
