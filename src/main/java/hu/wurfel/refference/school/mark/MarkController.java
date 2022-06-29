198c5803ba7bf5b3dd0ab9dff30dcabfb75eb93e
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-06-29 19:20:13 +0200

    MarkController @RequestMapping annotation added

--- src/main/java/hu/wurfel/refference/school/mark/MarkController.java
+++ src/main/java/hu/wurfel/refference/school/mark/MarkController.java
@@ -9,27 +9,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/Mark/")
 public class MarkController {
 
 
     MarkService markService;
 
-    @GetMapping("/api/v1/Mark/search/{searchWith}/{searchBy}/{value}")
+    @GetMapping("/search/{searchWith}/{searchBy}/{value}")
     public ResponseEntity<List<Mark>> searchForStudent(@PathVariable EntityNames searchWith, @PathVariable EntityFieldNames searchBy, @PathVariable String value) {
         return ResponseEntity.ok(markService.getSearchResponseList(searchWith, searchBy, value));
     }
 
-    @PostMapping("/api/v1/Mark/adding")
+    @PostMapping("/adding")
     public ResponseEntity<List<Mark>> create(@RequestBody MarkRequestForSearch markRequestForSearch) {
         return ResponseEntity.ok(markService.save(Long.parseLong(markRequestForSearch.getId()), Integer.parseInt(markRequestForSearch.getDiaryId()), markRequestForSearch.getDate(), Integer.parseInt(markRequestForSearch.getSubjectId()), Byte.parseByte(markRequestForSearch.getMark())));
     }
 
-    @DeleteMapping("/api/v1/Mark/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable String id) {
         markService.delete(markService.getByMarkId(Long.parseLong(id)));
     }
 
-    @PutMapping("/api/v1/Mark/modify")
+    @PutMapping("/modify")
     public ResponseEntity<List<Mark>> modifyStudent(@RequestBody MarkRequestForSearch markRequestForSearch) {
 	    Mark mark = markService.getByMarkId(Long.parseLong(markRequestForSearch.getId()));
         mark.setDiaryID(Integer.parseInt(markRequestForSearch.getDiaryId()));
