c29dcbfcff385ac609ba61d5492c66a03ccdae13
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-02 14:40:13 +0200

    StudentDto delete implementation

--- src/main/java/hu/wurfel/refference/school/subject/SubjectDto.java
+++ src/main/java/hu/wurfel/refference/school/subject/SubjectDto.java
@@ -2,10 +2,8 @@ package hu.wurfel.refference.school.subject;
 
 import lombok.Data;
 
-import java.io.Serializable;
-
 @Data
-public class SubjectDto implements Serializable {
+public class SubjectDto {
 	private final Integer id;
 	private final String name;
 	private final Long teacherId;
