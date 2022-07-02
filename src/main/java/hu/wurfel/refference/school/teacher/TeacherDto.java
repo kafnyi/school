db7cf194974943aad2ede05ec07f37b6c6201510
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-02 17:17:28 +0200

    TeacherDto birthDate type changed to String

--- src/main/java/hu/wurfel/refference/school/teacher/TeacherDto.java
+++ src/main/java/hu/wurfel/refference/school/teacher/TeacherDto.java
@@ -2,11 +2,9 @@ package hu.wurfel.refference.school.teacher;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class TeacherDto {
     private final Long id;
     private final String name;
-    private final Date birthDate;
+    private final String birthDate;
 }
