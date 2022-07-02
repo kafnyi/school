f88a0708da14e3ebd16e17fa89b1c689679cbf2a
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-02 14:36:50 +0200

    DiaryDto delete implementation

--- src/main/java/hu/wurfel/refference/school/diary/DiaryDto.java
+++ src/main/java/hu/wurfel/refference/school/diary/DiaryDto.java
@@ -2,10 +2,8 @@ package hu.wurfel.refference.school.diary;
 
 import lombok.Data;
 
-import java.io.Serializable;
-
 @Data
-public class DiaryDto implements Serializable {
+public class DiaryDto {
     private final Integer id;
     private final Long studentId;
     private final Integer classId;
