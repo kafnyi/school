c9d9c416d060c29f21d6f308379799631f5826e1
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-02 14:41:28 +0200

    TeacherDto delete implementation

--- src/main/java/hu/wurfel/refference/school/teacher/TeacherDto.java
+++ src/main/java/hu/wurfel/refference/school/teacher/TeacherDto.java
@@ -2,11 +2,10 @@ package hu.wurfel.refference.school.teacher;
 
 import lombok.Data;
 
-import java.io.Serializable;
 import java.util.Date;
 
 @Data
-public class TeacherDto implements Serializable {
+public class TeacherDto {
     private final Long id;
     private final String name;
     private final Date birthDate;
