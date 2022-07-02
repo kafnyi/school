cef922ae36761bbfe05dd7cda50afbd09f76cab4
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-02 16:06:29 +0200

    ClassController fixing

--- src/main/java/hu/wurfel/refference/school/division/ClassController.java
+++ src/main/java/hu/wurfel/refference/school/division/ClassController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Class/search/")
+@RequestMapping("/api/v1/Class/")
 public class ClassController {
 
     ClassService classService;
