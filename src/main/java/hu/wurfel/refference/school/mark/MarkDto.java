ae33766f8b5c12bb04d761bdb813afb0b6d71986
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-02 17:03:05 +0200

    MarkDto date tipe changed to String

--- src/main/java/hu/wurfel/refference/school/mark/MarkDto.java
+++ src/main/java/hu/wurfel/refference/school/mark/MarkDto.java
@@ -2,13 +2,11 @@ package hu.wurfel.refference.school.mark;
 
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class MarkDto {
     private final Long id;
     private final Integer diaryId;
-    private final Date date;
+    private final String date;
     private final Integer subjectId;
     private final byte mark;
 }
