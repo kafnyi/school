64fe898eda573e1771ecbf7cb11d5e8429bf4219
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-23 20:45:02 +0200

    toList method added

--- src/main/java/hu/wurfel/new_school_reference/diarySubjectTeacherStudent/DiarySubjectTeacherStudent.java
+++ src/main/java/hu/wurfel/new_school_reference/diarySubjectTeacherStudent/DiarySubjectTeacherStudent.java
@@ -10,15 +10,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"diary_id","subject_teacher_id", "student_id"}))
 public class DiarySubjectTeacherStudent extends Auditable {
 
     @ManyToOne
