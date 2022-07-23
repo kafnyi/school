ac78fda97edcffbcedd7f83b09e72e90f39b9961
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-23 22:25:26 +0200

    SearchService changes

--- src/main/java/hu/wurfel/new_school_reference/search/SearchService.java
+++ src/main/java/hu/wurfel/new_school_reference/search/SearchService.java
@@ -7,6 +7,7 @@ import hu.wurfel.new_school_reference.division.Class;
 import hu.wurfel.new_school_reference.division.ClassDto;
 import hu.wurfel.new_school_reference.division.ClassService;
 import hu.wurfel.new_school_reference.mark.MarkService;
+import hu.wurfel.new_school_reference.student.StudentDto;
 import hu.wurfel.new_school_reference.student.StudentService;
 import hu.wurfel.new_school_reference.subject.SubjectService;
 import hu.wurfel.new_school_reference.teacher.TeacherDto;
@@ -18,15 +19,14 @@ import java.util.List;
 @Service
 public class SearchService {
 
-	protected List<Class> findClassByClass;
 	private DiaryService diaryService;
 	private ClassService classService;
 	private StudentService studentService;
 	private SubjectService subjectService;
 	private MarkService markService;
+	private TeacherService teacherService;
 
 	//region Diary
-	private TeacherService teacherService;
 
 	public List<Diary> findDiaryByDiary(DiaryDto diaryDto) throws Exception{
 		return diaryService.findAllDiaryByDiaryIfNotEmpty(diaryDto);
@@ -39,6 +39,8 @@ public class SearchService {
 	public List<Diary> findDiaryByHeadTeacher(TeacherDto teacherDto) throws Exception{
 		return diaryService.findAllDiaryByTeacherIfNotEmpty(teacherDto);
 	}
+
+	//public List<Diary> findDiaryByStudent(StudentDto studentDto) {}
 
 	//region withClass
 
