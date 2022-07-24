7d17312bb8535e25fbec517e23699cfddb06818a
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-07-24 20:48:56 +0200

    StudentController created

---
+++ src/main/java/hu/wurfel/new_school_reference/student/StudentController.java
@@ -0,0 +1,33 @@
+package hu.wurfel.new_school_reference.student;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentController {
+
+	private final StudentService studentService;
+
+	@Autowired
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
+	@PostMapping
+	public ResponseEntity create(@RequestBody StudentDto studentDto) {
+		return this.studentService.save(studentDto);
+	}
+
+	@PutMapping
+	public ResponseEntity update(@RequestBody StudentDto studentDto) {
+		return this.studentService.save(studentDto);
+	}
+
+	@DeleteMapping
+	public void delete(@RequestBody StudentDto studentDto) {
+		this.studentService.deleteById(studentDto.getId());
+	}
+}
