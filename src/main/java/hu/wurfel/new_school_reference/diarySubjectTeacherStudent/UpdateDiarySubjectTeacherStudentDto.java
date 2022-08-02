cddefcfe1404f72f3dc5fb719dd4a3ed7d88457d
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-08-02 20:54:00 +0200

    DiarySubjectTeacherStudent refactor

---
+++ src/main/java/hu/wurfel/new_school_reference/diarySubjectTeacherStudent/UpdateDiarySubjectTeacherStudentDto.java
@@ -0,0 +1,35 @@
+package hu.wurfel.new_school_reference.diarySubjectTeacherStudent;
+
+import hu.wurfel.new_school_reference.base.BaseDto;
+import hu.wurfel.new_school_reference.base.Dto;
+import lombok.Data;
+
+@Data
+public class UpdateDiarySubjectTeacherStudentDto extends BaseDto {
+
+	private Long diaryId;
+	private Long subjectTeacherId;
+	private Long studentId;
+
+	public boolean hasValidDiaryId(){
+		return diaryId != null
+				&& diaryId > 0;
+	}
+
+	public boolean hasValidSubjectTeacherId(){
+		return subjectTeacherId != null
+				&& subjectTeacherId > 0;
+	}
+
+	public boolean hasValidStudentId(){
+		return studentId != null
+				&& studentId > 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return !this.hasValidDiaryId()
+				&& !this.hasValidSubjectTeacherId()
+				&& !this.hasValidStudentId();
+	}
+}
